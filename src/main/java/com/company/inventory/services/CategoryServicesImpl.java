963432aa083777e054a7ae4456d09e21afa3daff
Dennys Belduma <dennysbelduma98@gmail.com>
2023-10-13 13:13:13 -0500

    buscar categorias por id

--- src/main/java/com/company/inventory/services/CategoryServicesImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServicesImpl.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -37,6 +39,33 @@ public class CategoryServicesImpl implements ICategoryService{
 				return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 			return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		
+		CategoryResponseRest response =  new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		try {
+				Optional<Category> category = categoryDao.findById(id);
+				if(category.isPresent()) { 
+					list.add(category.get());
+					response.getCategoryResponse().setCategory(list);
+					response.setMetadata("Respuesta ok", "00", "Categoria encontrada");
+				}else {
+					response.setMetadata("Bad resquest", "404", "Categoria no encontrada");
+					return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+				}
+				
+			}catch(Exception e) {
+				
+				response.setMetadata("Bad resquest", "-1", "Error en la consulta por ID");
+				e.getStackTrace();
+				
+				return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
 	}
 
 }
